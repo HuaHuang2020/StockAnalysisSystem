7e83919827346f867d5c1c8adb03728f0e0c0cae
HuaHuang <459379028@qq.com>
2023-06-02 11:07:51 +0800

    first commit

---
+++ src/main/java/com/hh/StockAnalysisSystemApplication.java
@@ -0,0 +1,12 @@
+package com.hh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalysisSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockAnalysisSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hh/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.hh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author HH
+ * @Date 2023/6/2 10:43
+ */
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+}
